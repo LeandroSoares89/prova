ecc1f7a21994fd61dbc0ce1b51f928e15b594817
LeandroSoares89 <leandrosoarescosta@live.com>
2016-05-24 19:22:24 -0300

    alteraçao no insert e select

--- Prova/src/main/java/br/com/prova/model/EquipamentoDAO.java
+++ Prova/src/main/java/br/com/prova/model/EquipamentoDAO.java
@@ -2,6 +2,8 @@ package br.com.prova.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.prova.util.ConnectionFactory;
@@ -23,6 +25,8 @@ public class EquipamentoDAO extends ConnectionFactory implements CRUD{
 		} catch (Exception e) {
 			System.err.println("Erro: "+ e.getMessage());
 			e.printStackTrace();
+		}finally{
+			closeConnection(conexao, ps, null);
 		}
 		
 	}
@@ -41,8 +45,30 @@ public class EquipamentoDAO extends ConnectionFactory implements CRUD{
 
 	@Override
 	public List<Equipamento> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Equipamento> lsEquipamento = null;
+		Connection conexao = null;
+		PreparedStatement  ps = null;
+		ResultSet rs = null;
+		String sql = "SELECT codigo, nome, descricao"+
+		" FROM equipamento ORDER BY codigo";
+		
+		try {
+			conexao = openConnection();
+			ps = conexao.prepareStatement(sql);
+			rs= ps.executeQuery();
+			lsEquipamento = new ArrayList<Equipamento>();
+			while(rs.next()){
+				Equipamento equipamento = new Equipamento();
+				equipamento.setCodigo(rs.getLong("codigo"));
+				equipamento.setNome(rs.getString("nome"));
+				equipamento.setDescricao(rs.getString("descricao"));
+				lsEquipamento.add(equipamento);
+			}
+		} catch (Exception e) {
+			System.err.println("Erro: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return lsEquipamento;
 	}
 
 }
--- Prova/src/main/java/br/com/prova/web/MBEquipamento.java
+++ Prova/src/main/java/br/com/prova/web/MBEquipamento.java
@@ -1,48 +1,46 @@
 package br.com.prova.web;
 
-
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-
+import javax.faces.bean.SessionScoped;
 
 import br.com.prova.model.Equipamento;
 import br.com.prova.model.EquipamentoRN;
 
-@ManagedBean (name = "mbEquipamento")
+@ManagedBean(name = "mbEquipamento")
+@SessionScoped
+public class MBEquipamento {
 
-public class MBEquipamento  {
-
-	
-	
 	private Equipamento equipamento = new Equipamento();
-	
+
 	private List<Equipamento> lsequipamento = null;
 
 	// Getters e Setters
-	
-	
-		public String getListagemEquipamento(){
-			if(lsequipamento == null){
-				new EquipamentoRN().selectAll();
-			}
-			return "lsequipamento";
-			
-		}
-		public Equipamento getEquipamento() {
-			return equipamento;
-		}
 
-		public void setEquipamento(Equipamento equipamento) {
-			this.equipamento = equipamento;
-		}
-	
-	public String inserirEquipamento() {
+	public Equipamento getEquipamento() {
+		return equipamento;
+	}
+
+	public void setEquipamento(Equipamento equipamento) {
+		this.equipamento = equipamento;
+	}
+
+	public List<Equipamento> getListagemEquipamento() {
+		if(lsequipamento == null)
+			System.out.println("Passou...");
+			lsequipamento = new EquipamentoRN().selectAll();
+		return lsequipamento;
+
+	}
+	public String actionNovo(){
+		this.equipamento = new Equipamento();
+		return "form_equipamento";
+	}
+	public String actionSalvar() {
 		new EquipamentoRN().inserirEquipamento(equipamento);
 		this.lsequipamento = null;
 		return "index";
 	}
 
-	
-	
 }
